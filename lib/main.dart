04bf87064721c84fe33831e3530c48cabc1f4d75
gabriel-araujo <gabrielaraujo182@gmail.com>
2019-01-10 16:06:53 -0300

    Hiding slow mode banner

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,7 @@ class L2ssApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Lineage 2 Servers Status',
       theme: new ThemeData(primaryColor: Color.fromRGBO(16, 10, 6, 1.0)),
       home: new ListPage(title: 'Lineage 2 Servers Status'),
